0f34d9b3d2c9696f36fde10c51a0c9bc68d944a1
Lukas Olivier <olivier.lukas2003@gmail.com>
2026-05-26 18:53:29 +0200

    chore: added security linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginSecurity from "eslint-plugin-security";
+import noSecrets from "eslint-plugin-no-secrets";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  pluginSecurity.configs.recommended,
+  {
+    plugins: {
+      "no-secrets": noSecrets,
+    },
+    rules: {
+      "no-secrets/no-secrets": ["error", { tolerance: 4.5 }],
+      "security/detect-object-injection": "off",
+    },
+  },
   {
     ignores: [
       "node_modules/**",
